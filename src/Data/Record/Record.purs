faf0ef43ae583b281c6cf944df06bc86d1846e0e
kejace <kejace@gmail.com>
2017-12-21 18:51:52 -0800

    Now works on recursive records

--- src/Data/Record/Record.purs
+++ src/Data/Record/Record.purs
@@ -1,28 +1,52 @@
 module Data.Record.Pretty 
 (
   class PPRowList
+, class Pretty
 , ppRowList
 , ppRecord
+, pp
 )
 
 where
 
 import Prelude
 
+import Data.Array (fromFoldable)
+import Data.Foldable (class Foldable, fold, foldl)
+import Data.List.NonEmpty (fromList)
+import Data.List.Types (List(..), NonEmptyList(..))
+import Data.Maybe (Maybe(..))
 import Data.Record (get, delete)
 import Data.Symbol (class IsSymbol, SProxy(..), reflectSymbol)
+import Prettier.Printer (DOC, bracket, group, line, nest, nil, pretty, text)
 import Type.Prelude (class RowToList, class RowLacks)
 import Type.Row (kind RowList, Nil, Cons, RLProxy(..))
 
-import Prettier.Printer (DOC, bracket, group, line, nest, nil, pretty, text)
+class Pretty a where
+  pp :: a -> DOC
 
-class PPRow (row :: # Type) where
-  ppRow :: Record row -> DOC
+instance prettyFoldable ::
+  ( Pretty a
+  , Foldable f
+  ) => Pretty (f a) where
+  pp = pp <<< fromFoldable
 
-instance ppRowNil :: PPRow row where
-  ppRow rec = text "hllo"
+instance prettyArrayRow :: 
+  ( RowToList row list
+  , PPRowList list row
+  ) => Pretty (Record row) where
+  pp = ppRecord
 
-class PPRowList (list :: RowList) (row :: # Type) | list -> row where
+instance prettyArray :: (Pretty a) => Pretty (Array a) where
+  pp list = text "[" <> items <> text "]"
+    where
+    items = foldl cc nil list
+    cc b a = b <> text "," <> pp a
+
+instance prettyShow :: (Show a) => Pretty a where
+  pp = text <<< show
+
+class PPRowList (list :: RowList) (row :: #Type) | list -> row where
   ppRowList :: RLProxy list -> Record row -> DOC
 
 instance ppRowListNil :: PPRowList Nil () where
@@ -46,8 +70,8 @@ instance ppRowListConsRecord ::
     val = get key rec
     rest = ppRowList (RLProxy :: RLProxy listRest) (delete key rec)
 
-instance ppRowListConsShow ::
-  ( Show a
+instance ppRowListConsPretty ::
+  ( Pretty a
   , PPRowList listRest rowRest
   , RowCons  key a rowRest rowFull
   , RowLacks key rowRest
@@ -56,7 +80,7 @@ instance ppRowListConsShow ::
   ) => PPRowList (Cons key a listRest) rowFull where
   ppRowList _ rec = (text <<< reflectSymbol $ key)
                  <> text ":" 
-                 <> (text <<< show $ val)
+                 <> (pp val)
                  <> text ","
                  <> line
                  <> rest
@@ -71,6 +95,6 @@ ppRecord
   => PPRowList list row
   => Record row
   -> DOC
-ppRecord rec = bracket "{" rowListStrs  "}"
+ppRecord rec = bracket "{" rowListStrs "}"
   where
-  rowListStrs = ppRowList (RLProxy ::RLProxy list) rec
+  rowListStrs = ppRowList (RLProxy :: RLProxy list) rec
--- src/Main.purs
+++ src/Main.purs
@@ -7,15 +7,45 @@ import Prelude
 
 import Control.Monad.Eff.Console (log)
 import Prettier.Printer (pretty)
+import Ansi.Codes
+import Ansi.Output
+
 
 myRecord = {name: "alice", age: 45}
 
 author :: { name :: String, interests :: Array String }
 author =
     { name: "Phil"
-    , interests: ["Functional Programming", "JavaScript"]
+    , interests:
+      [ "Functional Programming"
+      , "JavaScript"
+      ]
     }
+
+nested =
+  { name: "bob"
+  , age: 23
+  , traits:
+    { lucky: { colors: ["red", "green"]
+             , number: 45
+             }
+    , unlucky: { signs: ["aquarius"]
+               , days: ["monday", "sunday"]
+               }
+    }
+  , hobbies:
+    [ { hobby: "fishing"
+      , daysActive: 4
+      }
+    , { hobby: "running"
+      , daysActive: 198
+      }
+    ]
+  }
 
 main = do
   log $ pretty 5 $ ppRecord myRecord
-  log $ pretty 5 $ ppRecord author -- 
+  log $ pretty 5 $ ppRecord author
+  log $ pretty 10 $ ppRecord nested
+  log $ withGraphics bold "hello"
+  log $ withGraphics (bold <> foreground Red) "something"
